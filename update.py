9cac721774a5cedc4a6f85c548ac3d68ab40ed12
Abhinandan <abhinandan.a1010@gmail.com>
2026-09-03 22:10:17 +0530

    Fix Fit-Guide project date and remove temporary script

--- update.py
+++
@@ -1,8 +0,0 @@
-with open('script.js', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-index = content.find('// --- 3D Background with Three.js ---')
-if index != -1:
-    content = content[:index]
-    with open('script.js', 'w', encoding='utf-8') as f:
-        f.write(content)
